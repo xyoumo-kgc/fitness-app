6caf6be3a1530dc91ccd36f0f4868c0ae00a8dd3
xyoumo-kgc <xyoumo@gmail.com>
2026-06-25 12:12:19 +0900

    筋トレ＆サプリ管理アプリ (PWA)

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'fit-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
